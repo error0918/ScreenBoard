ad0b29d801fe22d9991d7c8efec265a281ea24f3
error0918 <jtaeyeon05@gmail.com>
2022-12-09 15:40:08 +0900

    2022/12/09 15:40

--- app/src/main/java/com/taeyeon/screenboard/ui/ScreenBoardScreen.kt
+++ app/src/main/java/com/taeyeon/screenboard/ui/ScreenBoardScreen.kt
@@ -1,22 +1,41 @@
-@file:OptIn(ExperimentalAnimationApi::class)
+@file:OptIn(ExperimentalAnimationApi::class, ExperimentalMaterialApi::class,
+    ExperimentalFoundationApi::class
+)
 @file:Suppress("OPT_IN_IS_NOT_ENABLED")
 
 package com.taeyeon.screenboard.ui
 
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.FractionalThreshold
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.*
+import androidx.compose.material.rememberSwipeableState
+import androidx.compose.material.swipeable
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.DrawStyle
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.taeyeon.screenboard.R
@@ -39,7 +58,7 @@ fun ScreenBoardScreen() {
             layer2Color2 = Color(0xffffc0cb)
         )
     ) {
-        Controller()
+        //Controller()
 
         val viewModel = remember { MainViewModel() }
         val calendar by viewModel.time  .observeAsState(Calendar.getInstance())
@@ -47,6 +66,83 @@ fun ScreenBoardScreen() {
             calendar = calendar,
             modifier = Modifier.align(Alignment.Center)
         )
+
+        val hapticFeedback = LocalHapticFeedback.current
+
+        val swipeableState = rememberSwipeableState(
+            initialValue = 0,
+            confirmStateChange = { value ->
+                hapticFeedback.performHapticFeedback(HapticFeedbackType.TextHandleMove)
+                true
+            }
+        )
+
+        Box(
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .width(240.dp)
+                .height(48.dp)
+                .background(
+                    color = Color.Black.copy(alpha = 0.3f),
+                    shape = CircleShape
+                )
+                .border(
+                    width = 1.dp,
+                    color = Color.Black,
+                    shape = CircleShape
+                )
+                .swipeable(
+                    state = swipeableState,
+                    anchors = with(LocalDensity.current) {
+                        mapOf(
+                            0f to 0,
+                            240.dp.toPx() - 48.dp.toPx() to 1
+                        )
+                    },
+                    orientation = Orientation.Horizontal,
+                    thresholds = { _, _ -> FractionalThreshold(1f) },
+                    velocityThreshold = 100.dp
+                )
+        ) {
+            Canvas(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(
+                        vertical = 12.dp,
+                        horizontal = 24.dp
+                    ),
+                contentDescription = stringResource(id = R.string.clear_touch_protection_arrow)
+            ) {
+
+                for (i in 0..6) {
+                    drawPath(
+                        path = Path().apply {
+                            moveTo(30.dp.toPx() * i, 0f)
+                            lineTo(30.dp.toPx() * i + 12.dp.toPx(), size.height / 2)
+                            lineTo(30.dp.toPx() * i, size.height)
+                        },
+                        color = Color.White,
+                        alpha = 0.5f,
+                        style = Stroke(6.dp.toPx(), cap = StrokeCap.Round)
+                    )
+                }
+
+            }
+            Icon(
+                imageVector = Icons.Rounded.DoNotTouch,
+                contentDescription = stringResource(id = R.string.clear_touch_protection),
+                tint = Color.White,
+                modifier = Modifier
+                    .size(48.dp)
+                    .align(Alignment.CenterStart)
+                    .offset(with(LocalDensity.current) { swipeableState.offset.value.toDp() })
+                    .background(
+                        color = Color.Black.copy(alpha = 0.3f),
+                        shape = CircleShape
+                    )
+                    .padding(12.dp)
+            )
+        }
 
     }
 }
