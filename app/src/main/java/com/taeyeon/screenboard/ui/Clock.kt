82b5ede97ff993d1de7bdd6700a03d9ce564cce7
error0918 <jtaeyeon05@gmail.com>
2022-12-12 03:19:37 +0900

    2022/12/12 03:19

--- app/src/main/java/com/taeyeon/screenboard/ui/Clock.kt
+++ app/src/main/java/com/taeyeon/screenboard/ui/Clock.kt
@@ -7,15 +7,27 @@ import androidx.compose.ui.graphics.Color
 import com.taeyeon.screenboard.theme.clock
 import java.util.Calendar
 
+
 @Composable
 fun TextClock(
     calendar: Calendar,
     modifier: Modifier = Modifier
 ) {
     Text(
-        text = "${calendar.get(Calendar.HOUR_OF_DAY).let { if (it < 10) "0$it" else "$it"}}:${calendar.get(Calendar.MINUTE).let { if (it < 10) "0$it" else "$it"}}:${calendar.get(Calendar.SECOND).let { if (it < 10) "0$it" else "$it"}}.${calendar.get(Calendar.MILLISECOND)}",
+        text = "${getDigitNumber(calendar.get(Calendar.HOUR_OF_DAY), 2)}:${getDigitNumber(calendar.get(Calendar.MINUTE), 2)}:${getDigitNumber(calendar.get(Calendar.SECOND), 2)}.${calendar.get(Calendar.MILLISECOND) / 100}",
         style = clock,
         color = Color.Black,
         modifier = modifier
     )
+}
+
+
+fun getDigitNumber(number: Int, digits: Int): String {
+    return if (digits > 0) {
+        if (number.toString().length >= digits) {
+            number.toString().substring(0, digits)
+        } else {
+            "0".repeat(digits - number.toString().length) + number
+        }
+    } else ""
 }
